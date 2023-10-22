d13521da3301657aab9671afbeb086c1129fb950
raviatr <raviatr@gmail.com>
2023-10-21 20:38:30 -0700

    removed unwanted definitions

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,18 +1,2 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started 101smallsteps"
-fi
-
-python manage.py flush --no-input
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
-
-exec "$@"
