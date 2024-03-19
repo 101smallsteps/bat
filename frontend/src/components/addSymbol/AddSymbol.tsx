417c9728910cb676defeb3c0799e7fe63489055d
raviatr <raviatr@gmail.com>
2024-03-19 00:15:20 -0700

    fixed frontend

--- frontend/src/components/addSymbol/AddSymbol.tsx
+++ frontend/src/components/addSymbol/AddSymbol.tsx
@@ -28,7 +28,7 @@ const retrieveSymbols = async () => {
         console.log("token->"+tok);
         const backend_server = process.env.BACKEND_SERVER;
         const response = await axios.get(
-            "${backend_server}/api/fin/api/symbols/",
+            `${backend_server}/api/fin/api/symbols/`,
             {
                 'headers':{
                     "Content-Type": "application/json",
