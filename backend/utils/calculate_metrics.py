2b407ca3f9880450af1572e14ba495ea5581cdbb
raviatr <raviatr@gmail.com>
2024-03-31 22:00:03 -0700

    db update

--- backend/utils/calculate_metrics.py
+++ backend/utils/calculate_metrics.py
@@ -21,6 +21,16 @@ from financials.models import IncomeStatement  # Import your Django model
 from financials.models import Symbol  # Import your Django model
 from financials.models import Ratio  # Import your Django model
 
+# Initialize parser
+parser = argparse.ArgumentParser(description ="script to download data from Yfinance")
+parser.add_argument("-s", "--stmt_type", action="store", dest="model_name")
+parser.add_argument("-dt", "--data_type", action="store", dest="data_type")
+parser.add_argument("-ds", "--data_status", action="store", dest="data_status")
+parser.add_argument("-sym", "--symbol", action="store", dest="symbol")
+parser.add_argument("-df", "--data_frequency", action="store", dest="data_frequency")
+parser.add_argument("-dy", "--data_year", action="store", dest="data_year")
+parser.add_argument("-ip", "--input", action="store", dest="input")
+
 def read_csv(filename):
     data = {}
     with open(filename, 'r') as file:
@@ -75,16 +85,26 @@ def calculate_metric_from_income_stmt(fin_obj, csv_file_path):
 
 if __name__ == "__main__":
 
-#    if len(sys.argv) != 2:
-#        print("Usage: python script.py  output.csv")
-#        sys.exit(1)
-    arg_model_name="IncomeStatement"
-    arg_dataType="Q"
-    arg_dataStatus="CREATE"
-    arg_symbol="AAPL"
-    arg_dataFrequency="Q_1"
-    arg_dataYear=2024
-    input_file = sys.argv[1]
+##    if len(sys.argv) != 2:
+##        print("Usage: python script.py  output.csv")
+##        sys.exit(1)
+#    arg_model_name="IncomeStatement"
+#    arg_dataType="Q"
+#    arg_dataStatus="CREATE"
+#    arg_symbol="AAPL"
+#    arg_dataFrequency="Q_1"
+#    arg_dataYear=2024
+#    input_file = sys.argv[1]
+
+    args = parser.parse_args()
+
+    arg_model_name = args.model_name
+    arg_dataType = args.data_type
+    arg_dataStatus = args.data_status
+    arg_symbol = args.symbol
+    arg_dataFrequency = args.data_frequency
+    arg_dataYear = args.data_year
+    input_file = args.input
 
     fin_obj=get_financial_data(arg_dataType,arg_dataStatus,arg_symbol,arg_dataFrequency,arg_dataYear)
     if (arg_model_name == "IncomeStatement"):
