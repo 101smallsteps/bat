4f9c50baf4f82240b2a79d07efe53f9773670645
raviatr <raviatr@gmail.com>
2024-03-31 23:01:37 -0700

    fixed config

--- frontend/src/pages/analysis/Analysis.tsx
+++ frontend/src/pages/analysis/Analysis.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { useEffect,useState } from "react";
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import config from '../../config';
 
 
 import {
