3adebf4dcb78265987c522d842c77e95c38b8bba
raviatr <raviatr@gmail.com>
2024-04-16 22:31:06 -0700

    enable local mode setup

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,13 @@
 // config.ts
+//const config = {
+//    backend_server: 'https://core.bat4all.com' // Set your default API URL here
+//};
+
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://core.bat4all.com' : 'http://localhost:8080';
+
 const config = {
-    backend_server: 'https://core.bat4all.com' // Set your default API URL here
+    backend_server: `${baseURL}` // Set your default API URL here
 };
+
 
 export default config;
