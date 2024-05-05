9e76dca98dec9da62d194ddf9d661c8cf945776f
raviatr <raviatr@gmail.com>
2024-05-05 01:10:45 -0700

    added http config

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 #CORS_ORIGIN_ALLOW_ALL = False
-#CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOWED_ORIGINS = [
+CORS_ORIGIN_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOWED_ORIGINS = [
 # for react app
-    "http://localhost:5173",
-    'https://bat4all.com',
-    'https://core.bat4all.com',
-]
+#    "http://localhost:5173",
+#    'https://bat4all.com',
+#    'https://core.bat4all.com',
+#]
 
 CORS_ALLOW_HEADERS = [
     'Content-Type',
