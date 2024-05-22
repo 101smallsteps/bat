e5e00172aa74aa153b1b6dfe2a9e2537f6d0a068
raviatr <raviatr@gmail.com>
2024-05-21 18:52:29 -0700

    added nginx config without CORS

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -3,7 +3,7 @@
 //    backend_server: 'https://core.bat4all.com' // Set your default API URL here
 //};
 
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://core.bat4all.com' : 'http://localhost:8080';
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://bat4all.com' : 'http://localhost:8080';
 
 const config = {
     backend_server: `${baseURL}` // Set your default API URL here
